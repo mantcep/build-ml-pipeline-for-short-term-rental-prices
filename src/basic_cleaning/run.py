62bab4031671ad3732dc5c92dc1d57106c4846e3
Mantas Cepulkovskis <mantas.cepulkovskis@ibm.com>
2022-10-08 21:28:51 +0300

    Imporve cleaning component by removing properties with invalid geolocation

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -29,6 +29,10 @@ def go(args):
     # Convert last_review to datetime
     df['last_review'] = pd.to_datetime(df['last_review'])
 
+    logger.info("Removing properties with invalid geolocation.")
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
+
     logger.info("Saving cleaned dataset and uploading as an artifact.")
     df.to_csv(args.output_artifact, index=False)
     artifact = wandb.Artifact(
